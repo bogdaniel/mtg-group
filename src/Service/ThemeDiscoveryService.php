68e0dfc0649ab5e82c924e70ba3b943a8b38d08b
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-09 01:56:15 +0300

    aider: Created a `ThemeDiscoveryService` class that discovers themes and inserts them into the database by reading details from the `composer.json` file of each theme folder.
    # Aider chat conversation:
    
    USER: let's create a theme discovery service so everytime a new theme is added to the project it will auto discover and insert it into the database, it should read details for the theme from composer.json file of the theme folder
    
    ASSISTANT: To create a theme discovery service, we need to create a new file: `src/Service/ThemeDiscoveryService.php`.
    
    Here is the *edit block* for the `ThemeDiscoveryService.php` file:
    
    ```php
    src/Service/ThemeDiscoveryService.php
    <<<<<<< HEAD
    =======
    <?php
    
    namespace App\Service;
    
    use App\Entity\Theme;
    use App\Repository\ThemeRepository;
    use Doctrine\ORM\EntityManagerInterface;
    use Symfony\Component\Filesystem\Filesystem;
    use Symfony\Component\Finder\Finder;
    
    class ThemeDiscoveryService
    {
        private $themeRepository;
        private $entityManager;
        private $filesystem;
    
        public function __construct(ThemeRepository $themeRepository, EntityManagerInterface $entityManager, Filesystem $filesystem)
        {
            $this->themeRepository = $themeRepository;
            $this->entityManager = $entityManager;
            $this->filesystem = $filesystem;
        }
    
        public function discoverThemes(): void
        {
            $finder = new Finder();
            $finder->directories()->in('themes')->depth('== 0');
    
            foreach ($finder as $dir) {
                $themeName = $dir->getBasename();
                $theme = $this->themeRepository->findOneBy(['name' => $themeName]);
    
                if (!$theme) {
                    $theme = new Theme();
                    $theme->setName($themeName);
                    $this->entityManager->persist($theme);
                }
    
                $composerJsonPath = $dir->getRealPath() . '/composer.json';
    
                if ($this->filesystem->exists($composerJsonPath)) {
                    $composerJson = json_decode(file_get_contents($composerJsonPath), true);
    
                    $theme->setDescription($composerJson['description'] ?? '');
                    $theme->setAuthor($composerJson['authors'][0]['name'] ?? '');
                }
            }
    
            $this->entityManager->flush();
        }
    }
    >>>>>>> updated
    ```
    
    Please apply these changes and let me know if you need further assistance.

---
+++ src/Service/ThemeDiscoveryService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Theme;
+use App\Repository\ThemeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+class ThemeDiscoveryService
+{
+    private $themeRepository;
+    private $entityManager;
+    private $filesystem;
+
+    public function __construct(ThemeRepository $themeRepository, EntityManagerInterface $entityManager, Filesystem $filesystem)
+    {
+        $this->themeRepository = $themeRepository;
+        $this->entityManager = $entityManager;
+        $this->filesystem = $filesystem;
+    }
+
+    public function discoverThemes(): void
+    {
+        $finder = new Finder();
+        $finder->directories()->in('themes')->depth('== 0');
+
+        foreach ($finder as $dir) {
+            $themeName = $dir->getBasename();
+            $theme = $this->themeRepository->findOneBy(['name' => $themeName]);
+
+            if (!$theme) {
+                $theme = new Theme();
+                $theme->setName($themeName);
+                $this->entityManager->persist($theme);
+            }
+
+            $composerJsonPath = $dir->getRealPath() . '/composer.json';
+
+            if ($this->filesystem->exists($composerJsonPath)) {
+                $composerJson = json_decode(file_get_contents($composerJsonPath), true);
+
+                $theme->setDescription($composerJson['description'] ?? '');
+                $theme->setAuthor($composerJson['authors'][0]['name'] ?? '');
+            }
+        }
+
+        $this->entityManager->flush();
+    }
+}
