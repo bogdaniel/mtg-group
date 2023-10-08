e9b4ef1a811adf2cad56f9e9b274c5e780b9ab17
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-09 01:54:44 +0300

    aider: Created a command to generate a new theme folder with the specified structure.
    # Aider chat conversation:
    
    USER: create for me command to generate a new theme folder the theme folder name should be uppercase of every word inside of it we should have a composer.json file public folder templates folder translation folder inside templates we should have bundles folder
    
    ASSISTANT: To create a command to generate a new theme folder, we need to create a new file: `src/Command/GenerateThemeCommand.php`.
    
    Here is the *edit block* for the `GenerateThemeCommand.php` file:
    
    ```php
    src/Command/GenerateThemeCommand.php
    <<<<<<< HEAD
    =======
    <?php
    
    namespace App\Command;
    
    use Symfony\Component\Console\Command\Command;
    use Symfony\Component\Console\Input\InputInterface;
    use Symfony\Component\Console\Output\OutputInterface;
    use Symfony\Component\Console\Input\InputArgument;
    use Symfony\Component\Filesystem\Filesystem;
    
    class GenerateThemeCommand extends Command
    {
        protected static $defaultName = 'app:generate-theme';
    
        protected function configure()
        {
            $this
                ->setDescription('Generates a new theme folder.')
                ->addArgument('name', InputArgument::REQUIRED, 'The name of the theme.');
        }
    
        protected function execute(InputInterface $input, OutputInterface $output)
        {
            $filesystem = new Filesystem();
    
            $themeName = ucwords($input->getArgument('name'));
            $themeDir = 'themes/' . $themeName;
    
            $filesystem->mkdir([
                $themeDir,
                "$themeDir/public",
                "$themeDir/templates",
                "$themeDir/templates/bundles",
                "$themeDir/translations",
            ]);
    
            $filesystem->dumpFile("$themeDir/composer.json", '{}');
    
            $output->writeln("Theme $themeName generated successfully.");
    
            return Command::SUCCESS;
        }
    }
    >>>>>>> updated
    ```
    
    Please apply these changes and let me know if you need further assistance.

---
+++ src/Command/GenerateThemeCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Filesystem\Filesystem;
+
+class GenerateThemeCommand extends Command
+{
+    protected static $defaultName = 'app:generate-theme';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Generates a new theme folder.')
+            ->addArgument('name', InputArgument::REQUIRED, 'The name of the theme.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filesystem = new Filesystem();
+
+        $themeName = ucwords($input->getArgument('name'));
+        $themeDir = 'themes/' . $themeName;
+
+        $filesystem->mkdir([
+            $themeDir,
+            "$themeDir/public",
+            "$themeDir/templates",
+            "$themeDir/templates/bundles",
+            "$themeDir/translations",
+        ]);
+
+        $filesystem->dumpFile("$themeDir/composer.json", '{}');
+
+        $output->writeln("Theme $themeName generated successfully.");
+
+        return Command::SUCCESS;
+    }
+}
