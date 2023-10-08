9898309dd63f4d0b1a0d3f92a0d022d36a6d407f
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-09 02:04:28 +0300

    aider: Updated theme entity with getters and setters, and updated theme discovery service to throw an error when decoding JSON.
    # Aider chat conversation:
    
    ASSISTANT: I apologize for the confusion. Let's try again. Here are the correct changes:
    
    ```php
    src/Service/ThemeDiscoveryService.php
    <<<<<<< HEAD
                if ($this->filesystem->exists($composerJsonPath)) {
                    $composerJson = json_decode(file_get_contents($composerJsonPath), true);
    
                    $theme->setDescription($composerJson['description'] ?? '');
                    $theme->setAuthor($composerJson['authors'][0]['name'] ?? '');
                }
    =======
                if ($this->filesystem->exists($composerJsonPath)) {
                    $composerJson = json_decode(file_get_contents($composerJsonPath), true, 512, JSON_THROW_ON_ERROR);
    
                    $theme->setDescription($composerJson['description'] ?? '');
                    $theme->setAuthor($composerJson['authors'][0]['name'] ?? '');
                }
    >>>>>>> updated
    ```
    
    ```php
    src/Entity/Theme.php
    <<<<<<< HEAD
        // getters and setters...
    =======
        public function getId(): ?int
        {
            return $this->id;
        }
    
        public function getName(): ?string
        {
            return $this->name;
        }
    
        public function setName(string $name): self
        {
            $this->name = $name;
    
            return $this;
        }
    
        public function getDescription(): ?string
        {
            return $this->description;
        }
    
        public function setDescription(string $description): self
        {
            $this->description = $description;
    
            return $this;
        }
    
        public function getAuthor(): ?string
        {
            return $this->author;
        }
    
        public function setAuthor(string $author): self
        {
            $this->author = $author;
    
            return $this;
        }
    >>>>>>> updated
    ```
    
    Please confirm these changes so I can proceed.

--- src/Entity/Theme.php
+++ src/Entity/Theme.php
@@ -23,5 +23,44 @@ class Theme
     #[Column(type: "string", length: 255)]
     private $author;
 
-    // getters and setters...
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
 }
