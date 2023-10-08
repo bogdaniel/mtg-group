8d98b001765f7ee4a4618dc4bb4b1371c10a1592
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-09 02:05:30 +0300

    aider: Refactored `Theme` entity to be immutable by removing setter methods and setting properties in the constructor.
    # Aider chat conversation:
    
    USER: let's refactor entity to be immutable
    
    ASSISTANT: To refactor the `Theme` entity to be immutable, we need to make the following changes:
    
    1. Remove all setter methods from the `Theme` entity.
    2. Set all properties in the constructor.
    
    Here are the proposed changes:
    
    ```php
    src/Entity/Theme.php
    <<<<<<< HEAD
        #[Column(type: "string", length: 255)]
        private $name;
    
        #[Column(type: "string", length: 255)]
        private $description;
    
        #[Column(type: "string", length: 255)]
        private $author;
    
        public function getId(): ?int
        {
            return $this->id;
        }
    
        public function getName(): ?string
        {
            return $this->name;
        }
    
        public function setName(string $name): self
        {
            $this->name = $name;
    
            return $this;
        }
    
        public function getDescription(): ?string
        {
            return $this->description;
        }
    
        public function setDescription(string $description): self
        {
            $this->description = $description;
    
            return $this;
        }
    
        public function getAuthor(): ?string
        {
            return $this->author;
        }
    
        public function setAuthor(string $author): self
        {
            $this->author = $author;
    
            return $this;
        }
    =======
        #[Column(type: "string", length: 255)]
        private $name;
    
        #[Column(type: "string", length: 255)]
        private $description;
    
        #[Column(type: "string", length: 255)]
        private $author;
    
        public function __construct(string $name, string $description, string $author)
        {
            $this->name = $name;
            $this->description = $description;
            $this->author = $author;
        }
    
        public function getId(): ?int
        {
            return $this->id;
        }
    
        public function getName(): string
        {
            return $this->name;
        }
    
        public function getDescription(): string
        {
            return $this->description;
        }
    
        public function getAuthor(): string
        {
            return $this->author;
        }
    >>>>>>> updated
    ```
    
    Please confirm these changes so I can proceed.

--- src/Entity/Theme.php
+++ src/Entity/Theme.php
@@ -23,44 +23,30 @@ class Theme
     #[Column(type: "string", length: 255)]
     private $author;
 
+    public function __construct(string $name, string $description, string $author)
+    {
+        $this->name = $name;
+        $this->description = $description;
+        $this->author = $author;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getName(): string
     {
         return $this->name;
     }
 
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
+    public function getDescription(): string
     {
         return $this->description;
     }
 
-    public function setDescription(string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getAuthor(): ?string
+    public function getAuthor(): string
     {
         return $this->author;
-    }
-
-    public function setAuthor(string $author): self
-    {
-        $this->author = $author;
-
-        return $this;
     }
 }
