4dd0c53803d11d021feb62171fb5cd335a89e529
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-13 20:02:03 +0300

    aider: Added `__construct` method to the `ThemeDataContract` interface.
    # Aider chat conversation:
    
    USER: you edited the file wrong you put __construct outside the class
    
    ASSISTANT: I apologize for the mistake. The `__construct` method should indeed be inside the interface. Here is the corrected *edit block*:
    
    ```php
    src/Domain/Contract/ThemeDataContract.php
    <<<<<<< HEAD
    interface ThemeDataContract
    {
    }
    =======
    interface ThemeDataContract
    {
        public function __construct(
            string $name,
            string $title,
            string $description,
            string $author,
            string $version,
            bool $isActive
        );
    }
    >>>>>>> updated
    ```
    
    This *edit block* correctly adds the `version` parameter to the `__construct` method of the `ThemeDataContract` interface.

--- src/Domain/Contract/ThemeDataContract.php
+++ src/Domain/Contract/ThemeDataContract.php
@@ -4,6 +4,14 @@ namespace App\Domain\Contract;
 
 interface ThemeDataContract
 {
+    public function __construct(
+        string $name,
+        string $title,
+        string $description,
+        string $author,
+        string $version,
+        bool $isActive
+    );
 }
     public function __construct(
         string $name,
