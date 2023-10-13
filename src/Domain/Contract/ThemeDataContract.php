ae7eeb92740f00a94aeb73a50b936fb77381f832
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-13 20:01:00 +0300

    Remove unnecessary constructor parameters from ThemeDataContract.

--- src/Domain/Contract/ThemeDataContract.php
+++ src/Domain/Contract/ThemeDataContract.php
@@ -4,12 +4,4 @@ namespace App\Domain\Contract;
 
 interface ThemeDataContract
 {
-    public function __construct(
-        string $name,
-        string $title,
-        string $description,
-        string $author,
-        string $version,
-        bool $isActive
-    );
 }
