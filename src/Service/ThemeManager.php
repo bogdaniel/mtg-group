47b020973ae9d647e6b365373875fe7adddd2b1b
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-13 23:01:24 +0300

    Added methods to delete, find by name, find all, set active, and find by ID in the ThemeManager class.

--- src/Service/ThemeManager.php
+++ src/Service/ThemeManager.php
@@ -60,5 +60,42 @@ class ThemeManager
         throw new \RuntimeException('Theme not found');
     }
 
-    // ... rest of the code ...
+    public function deleteTheme(Theme $theme): void
+    {
+        $this->themeRepository->delete($theme);
+    }
+
+    public function findThemeByName(string $name): ?Theme
+    {
+        return $this->themeRepository->findThemeByName($name);
+    }
+
+    public function findAllThemes(): array
+    {
+        return $this->themeRepository->findAll();
+    }
+    
+    public function setActiveTheme(int $id): void
+    {
+        $theme = $this->findThemeById($id);
+        if ($theme) {
+            $updatedTheme = new Theme(
+                $theme->name,
+                $theme->title,
+                $theme->description,
+                $theme->authors,
+                $theme->version,
+                $theme->createdAt,
+                new \DateTime(),
+                1,
+                $id
+            );
+            $this->themeRepository->save($updatedTheme);
+        }
+    }
+
+    public function findThemeById(int $id): ?Theme
+    {
+        return $this->themeRepository->find($id);
+    }
 }
