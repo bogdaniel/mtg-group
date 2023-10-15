cb940f7f7af1e3ca32b71c45c98b69f52c691826
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-15 23:10:21 +0300

    Added QuestionsAnsweredEvent class to handle questions answered events with a package name property.

---
+++ src/Event/QuestionsAnsweredEvent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+class QuestionsAnsweredEvent extends Event
+{
+    public const NAME = 'theme.questions_answered';
+
+    protected string $packageName;
+
+    public function __construct(string $packageName)
+    {
+        $this->packageName = $packageName;
+    }
+
+    public function getPackageName(): string
+    {
+        return $this->packageName;
+    }
+}
