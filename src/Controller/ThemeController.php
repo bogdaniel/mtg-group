e7306f777fceee84961c866f75d84002239f738c
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-17 00:22:46 +0300

    Removed the unused `ThemeRuntimeConfigurator` dependency from the `ThemeController` constructor.

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Service\ThemeManager;
-use App\Service\ThemeRuntimeConfigurator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,14 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class ThemeController extends AbstractController
 {
     private ThemeManager $themeManager;
-    private ThemeRuntimeConfigurator $themeRuntimeConfigurator;
-
-    public function __construct(ThemeManager $themeManager, ThemeRuntimeConfigurator $themeRuntimeConfigurator)
+    public function __construct(ThemeManager $themeManager)
     {
         $this->themeManager = $themeManager;
-        $this->themeRuntimeConfigurator = $themeRuntimeConfigurator;
-        $this->themeRuntimeConfigurator->configure();
-
     }
 
     #[Route("/theme/switch/{themeId}", name: "theme_switch")]
