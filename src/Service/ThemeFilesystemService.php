d47b4d4e7f9c4ac330d6e00af78de8b5b2ee0244
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-20 00:19:37 +0300

    Refactor ThemeFilesystemService constructor to accept projectDir parameter and assign it to a private property.

--- src/Service/ThemeFilesystemService.php
+++ src/Service/ThemeFilesystemService.php
@@ -8,10 +8,12 @@ use Symfony\Component\Filesystem\Filesystem;
 class ThemeFilesystemService
 {
     private Filesystem $filesystem;
+    private string $projectDir;
 
-    public function __construct(Filesystem $filesystem)
+    public function __construct(Filesystem $filesystem, $projectDir)
     {
         $this->filesystem = $filesystem;
+        $this->projectDir = $projectDir;
     }
 
     public function createThemeDirectories(string $themeDir): void
@@ -32,14 +34,6 @@ class ThemeFilesystemService
         $this->filesystem->dumpFile("$themeDir/composer.json",
             json_encode($composerJson, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
         );
-    }
-
-    private string $projectDir;
-
-    public function __construct(Filesystem $filesystem, string $projectDir)
-    {
-        $this->filesystem = $filesystem;
-        $this->projectDir = $projectDir;
     }
 
     public function createChildThemeDirectoriesAndFiles(string $parentThemeName, ThemeDataContract $childThemeData): void
