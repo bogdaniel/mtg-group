86b5bc6d563146d3d1be0ce28257cfd167c0c949
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-21 12:17:56 +0300

    aider: Refactored `ThemeController` to have separate controllers per route.
    # Aider chat conversation:
    
    USER: let's refactor `ThemeController`
    
    ASSISTANT:
    ^C KeyboardInterrupt
    
    USER: let's refactor `ThemeController` and have one controller per route
    
    ASSISTANT: To refactor the `ThemeController` and have one controller per route, we will need to create separate controller classes for each route currently handled by `ThemeController`.
    
    Here are the new files we need to create:
    
    1. `src/Controller/SwitchThemeController.php`
    2. `src/Controller/DeactivateThemeController.php`
    3. `src/Controller/CreateChildThemeController.php`
    4. `src/Controller/InstallAssetsController.php`
    5. `src/Controller/ListThemesController.php`
    
    We will also need to modify the `src/Controller/ThemeController.php` file to remove the methods that we have moved to the new controller classes.
    
    Let's start with creating the `SwitchThemeController`. The new controller will look like this:
    
    ```php
    src/Controller/SwitchThemeController.php
    <<<<<<< HEAD
    
    =======
    <?php
    declare(strict_types=1);
    
    namespace App\Controller;
    
    use App\Service\ThemeManager;
    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
    use Symfony\Component\HttpFoundation\Response;
    use Symfony\Component\Routing\Annotation\Route;
    
    class SwitchThemeController extends AbstractController
    {
        private ThemeManager $themeManager;
        public function __construct(ThemeManager $themeManager)
        {
            $this->themeManager = $themeManager;
        }
    
        #[Route("/theme/switch/{themeId}", name: "theme_switch")]
        public function switchTheme(int $themeId): Response
        {
            $this->themeManager->setActiveTheme($themeId);
    
            return $this->redirectToRoute('themes');
        }
    }
    >>>>>>> updated
    ```
    
    Please confirm if this is the correct approach, and I will continue with the other controllers.

---
+++ src/Controller/SwitchThemeController.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Service\ThemeManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SwitchThemeController extends AbstractController
+{
+    private ThemeManager $themeManager;
+    public function __construct(ThemeManager $themeManager)
+    {
+        $this->themeManager = $themeManager;
+    }
+
+    #[Route("/theme/switch/{themeId}", name: "theme_switch")]
+    public function switchTheme(int $themeId): Response
+    {
+        $this->themeManager->setActiveTheme($themeId);
+
+        return $this->redirectToRoute('themes');
+    }
+}
