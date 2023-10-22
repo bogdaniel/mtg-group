3202a9383d3db63758be4a62deca829e98b6dbf1
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-22 14:21:06 +0300

    aider: Refactored ComposerJsonVisualizerController to use Twig templates for displaying the info.
    # Aider chat conversation:
    
    USER: let's refactor ComposerJsonVisualizerController so it will use twig to display the info
    
    ASSISTANT: To refactor the `ComposerJsonVisualizerController` to use Twig for displaying the information, we need to modify the `ComposerJsonVisualizerController.php` file. We will inject the `twig` service into the controller and use it to render a Twig template with the composer.json data.
    
    Here are the steps:
    
    1. Modify the `__construct` method to inject the `twig` service.
    2. Modify the `visualize` method to use the `twig` service to render a Twig template with the composer.json data.
    
    Here is the *edit block* for these changes:
    
    ```php
    src/Controller/ComposerJsonVisualizerController.php
    <<<<<<< HEAD
    <?php
    declare(strict_types=
    ^C KeyboardInterrupt
    
    USER: let's refactor ComposerJsonVisualizerController so it will use twig templates to display the info
    
    ASSISTANT: To refactor the `ComposerJsonVisualizerController` to use Twig for displaying the information, we need to modify the `ComposerJsonVisualizerController.php` file. We will inject the `twig` service into the controller and use it to render a Twig template with the composer.json data.
    
    Here are the steps:
    
    1. Modify the `__construct` method to inject the `twig` service.
    2. Modify the `visualize` method to use the `twig` service to render a Twig template with the composer.json data.
    
    Here is the *edit block* for these changes:
    
    ```php
    src/Controller/ComposerJsonVisualizerController.php
    <<<<<<< HEAD
    <?php
    declare(strict_types=1);
    
    namespace App\Controller;
    
    use App\Service\ComposerJsonVisualizer;
    use Symfony\Component\HttpFoundation\JsonResponse;
    
    class ComposerJsonVisualizerController
    {
        private ComposerJsonVisualizer $composerJsonVisualizer;
    
        public function __construct(ComposerJsonVisualizer $composerJsonVisualizer)
        {
            $this->composerJsonVisualizer = $composerJsonVisualizer;
        }
    
        public function visualize(): JsonResponse
        {
            $composerJsonData = $this->composerJsonVisualizer->visualize();
    
            return new JsonResponse($composerJsonData);
        }
    }
    =======
    <?php
    declare(strict_types=1);
    
    namespace App\Controller;
    
    use App\Service\ComposerJsonVisualizer;
    use Symfony\Component\HttpFoundation\Response;
    use Twig\Environment;
    
    class ComposerJsonVisualizerController
    {
        private ComposerJsonVisualizer $composerJsonVisualizer;
        private Environment $twig;
    
        public function __construct(ComposerJsonVisualizer $composerJsonVisualizer, Environment $twig)
        {
            $this->composerJsonVisualizer = $composerJsonVisualizer;
            $this->twig = $twig;
        }
    
        public function visualize(): Response
        {
            $composerJsonData = $this->composerJsonVisualizer->visualize();
    
            return new Response($this->twig->render('composer_json_visualizer.html.twig', ['composerJsonData' => $composerJsonData]));
        }
    }
    >>>>>>> updated
    ```
    
    Please note that you need to create a new Twig template `composer_json_visualizer.html.twig` in the appropriate directory (usually `templates/`) to display the composer.json data. The structure and design of this template depend on your specific requirements.

--- src/Controller/ComposerJsonVisualizerController.php
+++ src/Controller/ComposerJsonVisualizerController.php
@@ -4,21 +4,24 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Service\ComposerJsonVisualizer;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class ComposerJsonVisualizerController
 {
     private ComposerJsonVisualizer $composerJsonVisualizer;
+    private Environment $twig;
 
-    public function __construct(ComposerJsonVisualizer $composerJsonVisualizer)
+    public function __construct(ComposerJsonVisualizer $composerJsonVisualizer, Environment $twig)
     {
         $this->composerJsonVisualizer = $composerJsonVisualizer;
+        $this->twig = $twig;
     }
 
-    public function visualize(): JsonResponse
+    public function visualize(): Response
     {
         $composerJsonData = $this->composerJsonVisualizer->visualize();
 
-        return new JsonResponse($composerJsonData);
+        return new Response($this->twig->render('composer_json_visualizer.html.twig', ['composerJsonData' => $composerJsonData]));
     }
 }
