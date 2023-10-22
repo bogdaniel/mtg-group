4096b752e7c7c88c836513244e72bb7a9022447b
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-22 14:19:06 +0300

    aider: Created a new service `ComposerJsonVisualizer` and a new controller `ComposerJsonVisualizerController` to implement the feature of visualizing information from the `composer.json` file.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: themes/zenchron/default-theme/composer.json
    
    ASSISTANT: Great! Now that we have the `composer.json` file from the `zenchron/default-theme` directory, we can proceed with the changes.
    
    To implement the new feature to visualize the information from the `composer.json` file, we need to create a new service called `ComposerJsonVisualizer` that will parse the `composer.json` file and return the required information. We also need to create a new controller called `ComposerJsonVisualizerController` that will use this service and return the information in a suitable format.
    
    Here are the steps we need to follow:
    
    1. Create a new service `ComposerJsonVisualizer` in the `src/Service` directory.
    2. In the `ComposerJsonVisualizer` service, create a method `visualize` that will parse the `composer.json` file and return the required information.
    3. Create a new controller `ComposerJsonVisualizerController` in the `src/Controller` directory.
    4. In the `ComposerJsonVisualizerController`, create a method `visualize` that will use the `ComposerJsonVisualizer` service and return the information in a suitable format.
    
    Let's start with the `ComposerJsonVisualizer` service. Here is the code for the new service:
    
    ```php
    src/Service/ComposerJsonVisualizer.php
    <<<<<<< HEAD
    =======
    <?php
    declare(strict_types=1);
    
    namespace App\Service;
    
    class ComposerJsonVisualizer
    {
        private string $projectDir;
    
        public function __construct(string $projectDir)
        {
            $this->projectDir = $projectDir;
        }
    
        public function visualize(): array
        {
            $composerJsonPath = $this->projectDir . '/composer.json';
            $composerJson = json_decode(file_get_contents($composerJsonPath), true);
    
            return [
                'name' => $composerJson['name'] ?? null,
                'type' => $composerJson['type'] ?? null,
                'license' => $composerJson['license'] ?? null,
                'description' => $composerJson['description'] ?? null,
                'minimum-stability' => $composerJson['minimum-stability'] ?? null,
                'prefer-stable' => $composerJson['prefer-stable'] ?? null,
                'require' => $composerJson['require'] ?? [],
                'require-dev' => $composerJson['require-dev'] ?? [],
                'scripts' => $composerJson['scripts'] ?? [],
                'replace' => $composerJson['replace'] ?? [],
                'extra' => $composerJson['extra'] ?? [],
            ];
        }
    }
    >>>>>>> updated
    ```
    
    Next, let's create the `ComposerJsonVisualizerController`. Here is the code for the new controller:
    
    ```php
    src/Controller/ComposerJsonVisualizerController.php
    <<<<<<< HEAD
    =======
    <?php
    declare(strict_types=1);
    
    namespace App\Controller;
    
    use App\Service\ComposerJsonVisualizer;
    use Symfony\Component\HttpFoundation\JsonResponse;
    
    class ComposerJsonVisualizerController
    {
        private ComposerJsonVisualizer $composerJsonVisualizer;
    
        public function __construct(ComposerJsonVisualizer $composerJsonVisualizer)
        {
            $this->composerJsonVisualizer = $composerJsonVisualizer;
        }
    
        public function visualize(): JsonResponse
        {
            $composerJsonData = $this->composerJsonVisualizer->visualize();
    
            return new JsonResponse($composerJsonData);
        }
    }
    >>>>>>> updated
    ```
    
    These changes should implement the new feature to visualize the information from the `composer.json` file.

--- src/Controller/ComposerJsonVisualizerController.php
+++ src/Controller/ComposerJsonVisualizerController.php
@@ -22,3 +22,27 @@ class ComposerJsonVisualizerController
         return new JsonResponse($composerJsonData);
     }
 }
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Service\ComposerJsonVisualizer;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ComposerJsonVisualizerController
+{
+    private ComposerJsonVisualizer $composerJsonVisualizer;
+
+    public function __construct(ComposerJsonVisualizer $composerJsonVisualizer)
+    {
+        $this->composerJsonVisualizer = $composerJsonVisualizer;
+    }
+
+    public function visualize(): JsonResponse
+    {
+        $composerJsonData = $this->composerJsonVisualizer->visualize();
+
+        return new JsonResponse($composerJsonData);
+    }
+}
--- src/Service/ComposerJsonVisualizer.php
+++ src/Service/ComposerJsonVisualizer.php
@@ -32,3 +32,37 @@ class ComposerJsonVisualizer
         ];
     }
 }
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+class ComposerJsonVisualizer
+{
+    private string $projectDir;
+
+    public function __construct(string $projectDir)
+    {
+        $this->projectDir = $projectDir;
+    }
+
+    public function visualize(): array
+    {
+        $composerJsonPath = $this->projectDir . '/composer.json';
+        $composerJson = json_decode(file_get_contents($composerJsonPath), true);
+
+        return [
+            'name' => $composerJson['name'] ?? null,
+            'type' => $composerJson['type'] ?? null,
+            'license' => $composerJson['license'] ?? null,
+            'description' => $composerJson['description'] ?? null,
+            'minimum-stability' => $composerJson['minimum-stability'] ?? null,
+            'prefer-stable' => $composerJson['prefer-stable'] ?? null,
+            'require' => $composerJson['require'] ?? [],
+            'require-dev' => $composerJson['require-dev'] ?? [],
+            'scripts' => $composerJson['scripts'] ?? [],
+            'replace' => $composerJson['replace'] ?? [],
+            'extra' => $composerJson['extra'] ?? [],
+        ];
+    }
+}
