f767e9ef31e0d383279234694201763840866ce1
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-24 00:20:11 +0300

    Refactor asset manager and update import path for CSS file.

--- src/Service/AssetManager.php
+++ src/Service/AssetManager.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Domain\Contract\ThemeDataContract;
-use App\Domain\Entity\Theme;
-
 use Symfony\Component\Filesystem\Filesystem;
 
 class AssetManager
