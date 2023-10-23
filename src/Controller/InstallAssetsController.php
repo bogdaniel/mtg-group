ec49e625af6c3321194c1ba73a82a833fd625ce7
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-23 21:32:34 +0300

    aider: Add method to copy theme assets to project root in InstallAssetsController.
    # Aider chat conversation:
    
    USER: let's implement a new feature and we should use `src/Controller/InstallAssetsController.php`, the feature is to copy package.json and webpack.config.js from the active theme folder to the root dir of the project each time we activate a new theme
    
    ASSISTANT: To implement this feature, we need to modify the `src/Controller/InstallAssetsController.php` file. We will add a new method `copyThemeAssetsToProjectRoot` to handle the copying of `package.json` and `webpack.config.js` files from the active theme folder to the project root directory. This method will be called in the `__invoke` method after the theme installation logic.
    
    Here are the changes:
    
    ```php
    src/Controller/InstallAssetsController.php
    <<<<<<< HEAD
    class InstallAssetsController extends AbstractController
    {
        private ThemeManager $themeManager;
        public function __construct(ThemeManager $themeManager)
        {
            $this->themeManager = $themeManager;
        }
    
        #[Route("/theme/install/{themeId}", name: "theme_install")]
        public function __invoke(int $themeId): Response
        {
            $theme = $this->themeManager->findThemeById($themeId);
            if ($theme) {
                // logic to install theme assets...
            }
    
            return $this->redirectToRoute('themes');
        }
    }
    =======
    class InstallAssetsController extends AbstractController
    {
        private ThemeManager $themeManager;
        private string $projectDir;
    
        public function __construct(ThemeManager $themeManager, string $projectDir)
        {
            $this->themeManager = $themeManager;
            $this->projectDir = $projectDir;
        }
    
        #[Route("/theme/install/{themeId}", name: "theme_install")]
        public function __invoke(int $themeId): Response
        {
            $theme = $this->themeManager->findThemeById($themeId);
            if ($theme) {
                // logic to install theme assets...
                $this->copyThemeAssetsToProjectRoot($theme);
            }
    
            return $this->redirectToRoute('themes');
        }
    
        private function copyThemeAssetsToProjectRoot(Theme $theme): void
        {
            $themeDir = $this->projectDir . '/themes/' . $theme->getId();
            $filesToCopy = ['package.json', 'webpack.config.js'];
    
            foreach ($filesToCopy as $file) {
                if (file_exists($themeDir . '/' . $file)) {
                    copy($themeDir . '/' . $file, $this->projectDir . '/' . $file);
                }
            }
        }
    }
    >>>>>>> updated
    ```
    
    This code will copy the `package.json` and `webpack.config.js` files from the active theme directory to the project root directory each time a new theme is activated. Please note that this code assumes that the themes are located in the `themes` directory in the project root, and that each theme has its own directory named by its ID. If your directory structure is different, you will need to adjust the paths accordingly.

--- src/Controller/InstallAssetsController.php
+++ src/Controller/InstallAssetsController.php
@@ -11,9 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class InstallAssetsController extends AbstractController
 {
     private ThemeManager $themeManager;
-    public function __construct(ThemeManager $themeManager)
+    private string $projectDir;
+
+    public function __construct(ThemeManager $themeManager, string $projectDir)
     {
         $this->themeManager = $themeManager;
+        $this->projectDir = $projectDir;
     }
 
     #[Route("/theme/install/{themeId}", name: "theme_install")]
@@ -22,8 +25,21 @@ class InstallAssetsController extends AbstractController
         $theme = $this->themeManager->findThemeById($themeId);
         if ($theme) {
             // logic to install theme assets...
+            $this->copyThemeAssetsToProjectRoot($theme);
         }
 
         return $this->redirectToRoute('themes');
+    }
+
+    private function copyThemeAssetsToProjectRoot(Theme $theme): void
+    {
+        $themeDir = $this->projectDir . '/themes/' . $theme->getId();
+        $filesToCopy = ['package.json', 'webpack.config.js'];
+
+        foreach ($filesToCopy as $file) {
+            if (file_exists($themeDir . '/' . $file)) {
+                copy($themeDir . '/' . $file, $this->projectDir . '/' . $file);
+            }
+        }
     }
 }
