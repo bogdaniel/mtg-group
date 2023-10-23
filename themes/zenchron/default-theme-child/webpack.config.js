ffe1c513c94185b1397ab281184d29704756f8fe
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-23 23:15:28 +0300

    aider: Added entries for the `app.js` and `app.css` files in the `webpack.config.js` file.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: themes/zenchron/default-theme-child/assets/javascript/app.js
    
    ASSISTANT: Great! Now that we have the `app.js` file in `read-write` mode, we can proceed with the changes.
    
    To load the `app.js` and `app.css` files, we need to modify the `webpack.config.js` file. We will add two new entries for the `app.js` and `app.css` files in the `addEntry` method call.
    
    Here is the *edit block* for the change:
    
    ```javascript
    themes/zenchron/default-theme-child/webpack.config.js
    <<<<<<< HEAD
        .addEntry('app', './assets/javascript/app.js')
        .addEntry('app', './assets/css/app.css')
    =======
        .addEntry('app_js', './assets/javascript/app.js')
        .addEntry('app_css', './assets/css/app.css')
    >>>>>>> updated
    ```
    
    This change will make Webpack compile the `app.js` and `app.css` files and include them in the output bundle.

--- themes/zenchron/default-theme-child/webpack.config.js
+++ themes/zenchron/default-theme-child/webpack.config.js
@@ -20,8 +20,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/javascript/app.js')
-    .addEntry('app', './assets/css/app.css')
+    .addEntry('app_js', './assets/javascript/app.js')
+    .addEntry('app_css', './assets/css/app.css')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
