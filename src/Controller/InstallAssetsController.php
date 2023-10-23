5122fac4039474c98dda65cde6583d2f2907db03
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-23 21:35:10 +0300

    Add the use statement for the AssetManager service in InstallAssetsController.

--- src/Controller/InstallAssetsController.php
+++ src/Controller/InstallAssetsController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\AssetManager;
 use App\Service\ThemeManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
