dfbf0cc55d47d66dbe480577be4a7bf764687c00
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-26 22:41:12 +0300

    aider: Added /Users/m3tagh0st/code/ai-cms/src/Repository/DiskRepository.php

---
+++ src/Repository/DiskRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Disk;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class DiskRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Disk::class);
+    }
+
+    // Add methods for creating, reading, updating, and deleting Disk records
+}
