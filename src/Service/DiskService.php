3235baac1ad549e23ca76024172de2232e3c26c3
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-26 22:43:13 +0300

    Refactored DiskRepository and DiskService classes to remove unnecessary code duplication.

--- src/Service/DiskService.php
+++ src/Service/DiskService.php
@@ -33,20 +33,3 @@ class DiskService
         $this->diskRepository->delete($disk);
     }
 }
-<?php
-
-namespace App\Service;
-
-use App\Repository\DiskRepository;
-
-class DiskService
-{
-    private DiskRepository $diskRepository;
-
-    public function __construct(DiskRepository $diskRepository)
-    {
-        $this->diskRepository = $diskRepository;
-    }
-
-    // Add methods for creating, reading, updating, and deleting Disk records
-}
