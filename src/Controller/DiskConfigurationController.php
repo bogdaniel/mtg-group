1e271e1517710cae6a3d111f71a798b86a41ec29
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-10-27 02:13:08 +0300

    Add the FormBuilderService to the DiskConfigurationController.

--- src/Controller/DiskConfigurationController.php
+++ src/Controller/DiskConfigurationController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Form\DiskConfigurationType;
 use App\Service\DiskConfigurationManager;
+use App\Service\FormBuilderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
