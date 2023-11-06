d6be0e3ca566a1f6048ea1d4c68ab630fccc3637
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-11-06 21:51:45 +0200

    aider: Created a new class `BaseController` that extends `Symfony\Bundle\FrameworkBundle\Controller\AbstractController` and added common methods for controllers.
    # Aider chat conversation:
    
    USER: let's refactor a little and create a new class that will extend Symfony\Bundle\FrameworkBundle\Controller\AbstractController
    
    ASSISTANT: Sure, we can create a new class that extends `Symfony\Bundle\FrameworkBundle\Controller\AbstractController`. This class can be used as a base controller for other controllers in your application.
    
    Here is the new class:
    
    ```php
    src/Controller/BaseController.php
    <<<<<<< HEAD
    =======
    <?php
    
    namespace App\Controller;
    
    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
    
    class BaseController extends AbstractController
    {
        // Add common methods for controllers here
    }
    >>>>>>> updated
    ```
    
    This will create a new file `BaseController.php` in the `src/Controller` directory. The `BaseController` class extends `AbstractController` from Symfony. You can add common methods for controllers in this class.

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BaseController extends AbstractController
+{
+    // Add common methods for controllers here
+}
