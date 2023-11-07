59f8d32e8ab464a835b779afe8cb3c7b85412d4c
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-11-07 21:50:22 +0200

    latest

--- src/Controller/StaticPageController.php
+++ src/Controller/StaticPageController.php
@@ -2,24 +2,12 @@
 
 namespace App\Controller;
 
-use App\Service\ThemeManager;
-use App\Service\ThemeService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class StaticPageController extends AbstractController
 {
-    private ThemeService $themeService;
-    private ThemeManager $themeManager;
-
-    public function __construct(ThemeService $themeService, ThemeManager $themeManager)
-    {
-        $this->themeService = $themeService;
-        $this->themeManager = $themeManager;
-    }
-
     #[Route("/", name: "home")]
     public function home(): Response
     {
@@ -29,9 +17,6 @@ class StaticPageController extends AbstractController
     #[Route("/page/{pageName}", name: "static_page", defaults: ["pageName" => "home"])]
     public function loadPage(string $pageName = 'home'): Response
     {
-        $activeTheme = $this->themeManager->getActiveTheme();
-        $content = $this->themeService->loadStaticPage($activeTheme->name, $pageName);
-
-        return new Response($content);
+        return $this->render("templates/pages/{$pageName}.html.twig");
     }
 }
