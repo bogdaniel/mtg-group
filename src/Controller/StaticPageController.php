131e3677cbfcb2152be86f782a89d6f06f8b969a
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-11-07 23:38:43 +0200

    latest

--- src/Controller/StaticPageController.php
+++ src/Controller/StaticPageController.php
@@ -17,6 +17,38 @@ class StaticPageController extends AbstractController
     #[Route("/page/{pageName}", name: "static_page", defaults: ["pageName" => "home"])]
     public function loadPage(string $pageName = 'home'): Response
     {
-        return $this->render("templates/pages/{$pageName}.html.twig");
+        $multiGamaGroupMenu = [
+            [
+                'name' => 'Acasa',
+                'path' => '/',
+            ],
+            [
+                'name' => 'Despre Noi',
+                'path' => 'pages/about-us'
+            ],
+            [
+                'name' => 'Produse',
+                'path' => '/pages/products'
+            ],
+            [
+                'name' => 'Servicii',
+                'path' => '/pages/services/'
+            ],
+            [
+                'name' => '',
+                'path' => '/'
+            ],
+            [
+                'name' => 'Proiecte',
+                'path' => '/pages/projects'
+            ],
+            [
+                'name' => 'Contact',
+                'path' => '/pages/contact'
+            ],
+        ];
+        return $this->render("templates/pages/{$pageName}.html.twig", [
+            'multiGamaGroupMenu' => $multiGamaGroupMenu
+        ]);
     }
 }
