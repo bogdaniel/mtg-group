93e01afff8e35d6bf00eb4409a00c18c95eb7cce
Bogdan Olteanu <bogdan@zenchron.com>
2023-11-16 20:46:56 +0200

    latest

--- src/Kernel.php
+++ src/Kernel.php
@@ -13,54 +13,54 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel implements CompilerPassInterface
+class Kernel extends BaseKernel // implements CompilerPassInterface
 {
     use MicroKernelTrait;
 
-    private MediaBundleExtension $extension;
+//    private MediaBundleExtension $extension;
 
     #[NoReturn]
-    public function process(ContainerBuilder $container): void
-    {
-        $typesDefinition = [];
-        if ($container->hasParameter('doctrine.dbal.connection_factory.types')) {
-            $typesDefinition = $container->getParameter('doctrine.dbal.connection_factory.types');
-        }
-
-        $taggedEnums = $container->findTaggedServiceIds('app.doctrine_enum_type');
-
-        /** @var $enumType AbstractEnumType */
-        foreach ($taggedEnums as $enumType => $definition) {
-            $typesDefinition[$enumType::NAME] = ['class' => $enumType];
-        }
-        $container->setParameter('doctrine.dbal.connection_factory.types', $typesDefinition);
-    }
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-        $container->addCompilerPass(new MediaCompilerPass()); // PassConfig::TYPE_REMOVE
-        if (\class_exists(DoctrineOrmMappingsPass::class))
-        {
-            $container->addCompilerPass(
-                DoctrineOrmMappingsPass::createAttributeMappingDriver(
-                    ['App\Domain'],
-                    [\dirname(__DIR__).'/src/Domain']
-                )
-            );
-        }
-    }
+//    public function process(ContainerBuilder $container): void
+//    {
+//        $typesDefinition = [];
+//        if ($container->hasParameter('doctrine.dbal.connection_factory.types')) {
+//            $typesDefinition = $container->getParameter('doctrine.dbal.connection_factory.types');
+//        }
+//
+//        $taggedEnums = $container->findTaggedServiceIds('app.doctrine_enum_type');
+//
+//        /** @var $enumType AbstractEnumType */
+//        foreach ($taggedEnums as $enumType => $definition) {
+//            $typesDefinition[$enumType::NAME] = ['class' => $enumType];
+//        }
+//        $container->setParameter('doctrine.dbal.connection_factory.types', $typesDefinition);
+//    }
+//    public function build(ContainerBuilder $container): void
+//    {
+//        parent::build($container);
+//        $container->addCompilerPass(new MediaCompilerPass()); // PassConfig::TYPE_REMOVE
+//        if (\class_exists(DoctrineOrmMappingsPass::class))
+//        {
+//            $container->addCompilerPass(
+//                DoctrineOrmMappingsPass::createAttributeMappingDriver(
+//                    ['App\Domain'],
+//                    [\dirname(__DIR__).'/src/Domain']
+//                )
+//            );
+//        }
+//    }
 
     /**
      * @return \Symfony\Component\DependencyInjection\Extension\ExtensionInterface|null
      */
-    public function getContainerExtension(): ?ExtensionInterface
-    {
-        if (null === $this->extension) {
-            $this->extension = new MediaBundleExtension();
-        }
-
-        return $this->extension;
-    }
+//    public function getContainerExtension(): ?ExtensionInterface
+//    {
+//        if (null === $this->extension) {
+//            $this->extension = new MediaBundleExtension();
+//        }
+//
+//        return $this->extension;
+//    }
 
 
     public function getPath(): string
