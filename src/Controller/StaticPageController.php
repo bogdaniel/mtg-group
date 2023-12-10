203e35ae6f0b733ac1bb76ec3ea5e38f17a7b4ce
Bogdan@zenchron.com <bogdan@zenchron.com>
2023-12-11 00:42:36 +0200

    latest

--- src/Controller/StaticPageController.php
+++ src/Controller/StaticPageController.php
@@ -172,10 +172,10 @@ grupuril de pompare, stații de pompare și echipamentele de stingere a incendii
                     'name' => 'Despre Noi',
                     'path' => '/multigama-tech-about-us/p',
                 ],
-//                [
-//                    'name' => 'Produse',
-//                    'path' => '/multigama-tech-products/p'
-//                ],
+                [
+                    'name' => 'Produse',
+                    'path' => '/multigama-tech-products/p'
+                ],
                 [
                     'name' => 'Servicii',
                     'path' => '/multigama-tech-services/p',
@@ -343,15 +343,15 @@ Specializați în furnizarea unei game complete de pompe industriale, de la cele
                     'link' => '/multigama-service-home/p',
                 ],
                 [
-                    'icon' => 'arrow-up-from-water-pump', // Font Awesome icon class
-                    'title' => 'Multigama Tech',
-                    'description' => 'Cu o experiență vastă și o dedicare neclintită față de excelență, Multigama Tech se află în fruntea industriei de vânzare a pompelor industriale. Suntem pasionați de furnizarea soluțiilor ideale pentru nevoile de transport și gestionare a fluidelor în industrie.',
-                    'link' => '/multigama-tech-home/p',
+                    'icon' => 'flaticon-agronomy', // Font Awesome icon class
+                    'title' => 'Furnizare echipamente noi',
+                    'description' => 'Furnizare echipamente noi, performante si eficiente din punct de vedere hidraulic și energetic. Furnizare piese de schimb pe toată durata de viață a echipamentelor',
+                    'link' => '/multigama-service-home/p',
                 ],
                 [
-                    'icon' => 'fire-extinguisher', // Font Awesome icon class
-                    'title' => 'EQ Fire',
-                    'description' => 'EQ Fire se remarcă ca lider de încredere în furnizarea și întreținerea sistemelor de stingere a incendiilor pentru mediul industrial. Ne angajăm să asigurăm protecția optimă împotriva incendiilor în facilitățile industriale, oferind soluții inovatoare și servicii de cea mai înaltă calitate.',
+                    'icon' => 'flaticon-crane', // Font Awesome icon class
+                    'title' => 'Suport tehnic',
+                    'description' => 'Suport tehnic în alegerea și dimensionarea echipamentelor pentru diverse tipuri de aplicații în funcție de necesitatea clientului. Suport tehnic în vederea înlocuirii echipamentelor vechi și neperformante etc.',
                     'link' => '/multigama-eq-fire-home/p',
                 ],
             ];
